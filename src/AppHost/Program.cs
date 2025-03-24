6056ef9455a0d8121ce9def0829f9d961412344a
404NotFoundGenius <152699846+404NotFoundGenius@users.noreply.github.com>
2025-03-24 15:08:01 +0100

    update on set up Authentication Service Scaffold

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -1,3 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var api = builder.AddProject<Projects.DanceChoreographyManager_Services_Authentication>("authentication");
+
 builder.Build().Run();
